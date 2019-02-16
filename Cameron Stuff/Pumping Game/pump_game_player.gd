cac3b6e4a6cc0061ae5de8669fcc6e61e456d273
Cameron <hagaboogaplease@gmail.com>
2019-02-16 04:07:53 -0800

    Moving inputs for pumping game
    Moving inputs for pumping game logic

--- Cameron Stuff/Pumping Game/pump_game_player.gd
+++ Cameron Stuff/Pumping Game/pump_game_player.gd
@@ -3,14 +3,18 @@ extends Node2D
 var pump_counter = 0
 var player_child
 var pump_child
+#var input_box
 
 func _ready():
-	pump_child = get_child(0)
+	player_child = get_child(0)
 	pump_child = get_child(1)
+	#input_box = get_child(2)
 		
 func pump_down():
 	pump_child.play("PumpDown")
-	pump_counter += 1
-
+	
 func pump_up():
 	pump_child.play("PumpUp")
+
+func _on_PumpSprite_animation_finished():
+	pump_child.stop()
