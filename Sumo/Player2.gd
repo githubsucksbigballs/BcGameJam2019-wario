b1c3542d72d03c81a2686b67f5f5653cf9e116fa
yoshiumw <wongyoshium@gmail.com>
2019-02-16 09:47:37 -0800

    Sumo: Asteroidsd implemented

--- Sumo/Player2.gd
+++ Sumo/Player2.gd
@@ -44,9 +44,12 @@ func _physics_process(delta):
 	if collision:
 		hitstun = true
 		get_parent().get_node("HitStun2").start()
-		get_parent().get_node("LoopFix2").start()
 		if(collision.collider.has_method("hit")):
+			get_parent().get_node("LoopFix2").start()
 			get_parent().get_node("Player3").hit(velocity*2)
+		if(collision.collider.has_method("isAsteroid")):
+			velocity = -velocity
+		
 
 func _on_HitStun2_timeout():
 	hitstun = false
@@ -61,6 +64,7 @@ func _on_VisibilityNotifier2D_screen_exited():
 	get_parent().get_node("MessageLabel").text = "Arrow Key Player\n has won!"
 	get_parent().get_node("MessageLabel").show()
 	get_parent().get_node("Player3").hitstun = true
+	get_parent().get_node("MobTimer").stop()
 	queue_free()
 
 
