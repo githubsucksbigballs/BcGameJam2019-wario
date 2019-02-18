5d32af7833b7dbf1940cc662afcc7b9c956fa008
yoshiumw <wongyoshium@gmail.com>
2019-02-17 16:38:05 -0800

    FINAL

--- Cameron Stuff/Main Menu/MainMenu.gd
+++ Cameron Stuff/Main Menu/MainMenu.gd
@@ -6,14 +6,14 @@ func _on_Quit_pressed():
 
 func _on_2_Player_pressed():
 	transition.fade_to("res://Astroid/Main.tscn")
-	player1meteor = 0
-	player1pump = 0
-	player1balloon= 0
-	player1sumo= 0
-	player1total= 0
+	Scores.player1meteor = 0
+	Scores.player1pump = 0
+	Scores.player1balloon= 0
+	Scores.player1sumo= 0
+	Scores.player1total= 0
 	
-	player2meteor= 0
-	player2pump= 0
-	player2balloon= 0
-	player2sumo= 0
-	player2total= 0
+	Scores.player2meteor= 0
+	Scores.player2pump= 0
+	Scores.player2balloon= 0
+	Scores.player2sumo= 0
+	Scores.player2total= 0
